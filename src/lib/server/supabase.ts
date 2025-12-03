a45c89d30bdd8d7aaaf3e6bca7d1c3e110a4eecf
Terje-Monsen <tm@norsktest.no>
2025-12-03 09:06:43 +0100

    Lagt til ny db key og oppdatert vercel med verdi

--- src/lib/server/supabase.ts
+++ src/lib/server/supabase.ts
@@ -2,5 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
 
-// Bruk service role key KUN på server (aldri i frontend)
-export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
+  auth: {
+    persistSession: false
+  }
+});
