b095ee2dc18427d7bd7c75098d4dbb096838b5cb
Terje-Monsen <tm@norsktest.no>
2025-12-03 09:24:58 +0100

    Oppdatert vercel-instilinger db.

--- src/lib/server/supabase.ts
+++ src/lib/server/supabase.ts
@@ -2,7 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
 
-// Server-only klient (bruker service role)
+
+// Server-only klient (bruker service role-)
 export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
   auth: {
     persistSession: false
